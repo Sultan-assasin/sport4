77614d432f1ee6c71fa6ffd5e76e6a32ea9918ae
Sultan-assasin <sulltanadai@gmail.com>
2023-07-31 11:21:57 +0600

    Initial commit

---
+++ app/src/main/java/com/sula/sport4/ui/theme/Color.kt
@@ -0,0 +1,13 @@
+package com.sula.sport4.ui.theme
+
+import android.graphics.Color.rgb
+import androidx.compose.ui.graphics.Color
+
+val Purple200 = Color(0xFFBB86FC)
+val Purple500 = Color(0xFF6200EE)
+val Purple700 = Color(0xFF3700B3)
+val Teal200 = Color(0xFF03DAC5)
+
+val SoftBlue = Color(0xFF1D5F59)
+val ZAZA = Color(rgb(26,72,149))
+val VS = Color(rgb(0,9,87))
